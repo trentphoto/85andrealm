ca7331774c85b32667a25bd64b604b174dc7c9c5
trentphoto <trentphoto@gmail.com>
2023-04-11 21:27:37 -0400

    escape single quotes on about page, remove customtypes file

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -11,8 +11,8 @@ export default function AboutPage() {
                 <div className="text-gray-500">
                     <p className="">Welcome to 85 + REALM. This is where our passion for flowers meets your passion for flowers.</p>
                     <p>As a family-owned and operated business, we are entirely family-owned and operated.</p>
-                    <p>We have been helping people's lives blossom with our stunning flower arrangements and handpicked bouquets since 1998. Our talented team of florists believes in the power of flowers to create an atmosphere of joy, elegance, and love, and we're excited to share that with you.</p>
-                    <p>We use humans to write most of our content, including our About pages. You won't find any AI-generated blog posts on this site, unless you look really hard.</p>
+                    <p>We have been helping people&#39;s lives blossom with our stunning flower arrangements and handpicked bouquets since 1998. Our talented team of florists believes in the power of flowers to create an atmosphere of joy, elegance, and love, and we&#39;re excited to share that with you.</p>
+                    <p>We use humans to write most of our content, including our About pages. You won&#39;t find any AI-generated blog posts on this site, unless you look really hard.</p>
                 </div>
                 <ButtonLink href="/shop" className="mt-12">View our Incredible Product Lineup</ButtonLink>
             </div>
--- src/types/customTypes.ts
+++
@@ -1,7 +0,0 @@
-import { NextPageContext } from 'next';
-import { Store } from 'redux';
-import { RootState } from '../lib/redux';
-
-export interface CustomPageContext extends NextPageContext {
-  store: Store<RootState>;
-}
